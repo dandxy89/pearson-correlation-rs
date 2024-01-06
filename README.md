34cc10d2e38af52e31a050e826d9775592c318e8
Dan Dixey <dan@sigmafinancial.ai>
2024-01-06 20:55:11 +0000

    fix: Init the project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pearson Correlation SIMD
+
+## Overview
+
+Simple implementation of the Pearson Correlation algorithm in `Rust` using `SIMD` instructions.
+
+It is intended solely as a project to familiarize myself with the use of SIMD instructions in `Rust` via the `packed_simd` crate.
+
+## Tests
+
+TODO - Will add tests in the week.
---
+++ src/corrcoef_f64.rs
@@ -0,0 +1,47 @@
+use packed_simd::f64x4;
+
+const SIZE: usize = 4;
+
+/// The Pearson correlation coefficient measures the linear relationship between
+/// the two sequences of values.
+///
+/// It is equivalent to the numpy function `np.corrcoef`.
+///
+/// # Arguments
+/// * `seq1` - The first sequence
+/// * `seq2` - The second sequence
+///
+fn corrcoef_f64x4(seq1: &[f64], seq2: &[f64]) -> f64 {
+    let seq_len = seq1.len();
+    assert_eq!(seq_len, seq2.len());
+
+    let mut sum_sq1 = f64x4::splat(0.0);
+    let mut sum_sq2 = f64x4::splat(0.0);
+    let mut dot_product = f64x4::splat(0.0);
+
+    for i in (0..seq_len).step_by(SIZE) {
+        if i + SIZE > seq_len {
+            break;
+        }
+        let vec1 = f64x4::from_slice_unaligned(&seq1[i..i + SIZE]);
+        let vec2 = f64x4::from_slice_unaligned(&seq2[i..i + SIZE]);
+
+        dot_product += vec1 * vec2;
+        sum_sq1 += vec1 * vec1;
+        sum_sq2 += vec2 * vec2;
+    }
+
+    let mut sum_sq1_sum = sum_sq1.extract(0) + sum_sq1.extract(1);
+    let mut sum_sq2_sum = sum_sq2.extract(0) + sum_sq2.extract(1);
+    let mut dot_product_sum = dot_product.extract(0) + dot_product.extract(1);
+
+    // Handle remaining elements (if any || seq_len % SIZE != 0)
+    for i in (seq_len - seq_len % SIZE)..seq_len {
+        dot_product_sum += seq1[i] * seq2[i];
+        sum_sq1_sum += seq1[i] * seq1[i];
+        sum_sq2_sum += seq2[i] * seq2[i];
+    }
+
+    let denom = sum_sq1_sum.sqrt() * sum_sq2_sum.sqrt();
+    dot_product_sum / denom
+}
